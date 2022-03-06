00804766ee6e57c7dddc5ee00cdbeaf5dfa63198
codeinsack <codeinsack@gmail.com>
2022-03-06 16:16:25 +0300

    add shared modules

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-import faker from "faker";
-
-let products = "";
-
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
-
-document.querySelector("#products-dev").innerHTML = products;
+import("./bootstrap");
